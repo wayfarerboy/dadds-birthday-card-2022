b815aa4c95a0807fc3171b7acf8c3d0ea382b1ac
Al Pagan <generamics@gmail.com>
2022-06-18 14:15:34 +0100

    Corrected links

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,9 @@ export default function Home() {
     <p>I&apos;m thinking this will help you to pass those relaxing evenings in your apartment, sketching and painting away on your iPad! It should work well as an accompaniment to your book (which will be waiting for you when you get back home).</p>
     <p className={classes.last}>Hope you have an ace day. Love you. Al xxx</p>
     <div className={classes.buttons}>
-    <Link external  target="_blank" href="https://pay.monzo.com/#F04EGRf_rHpfTiup1FH8WjIOge2MODAqZLWyNwdYr6Sn558nyzJteL25Ng%3D%3D">Money from me</Link>
+    <a target="_blank" rel="noreferrer" href="https://pay.monzo.com/#F04EGRf_rHpfTiup1FH8WjIOge2MODAqZLWyNwdYr6Sn558nyzJteL25Ng%3D%3D">Money from me</a>
     <p className={classes.topay}>to pay for</p>
-    <Link external target="_blank" href="https://www.udemy.com/share/1013fe/">whatever you fancy from here</Link>
+    <a target="_blank" rel="noreferrer" href="https://www.udemy.com/share/1013fe/">whatever you fancy from here</a>
     </div>
   </div>
 </div>
