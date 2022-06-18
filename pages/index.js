4615e01fb5ee5176380bf2defd6205bb958a0dee
Al Pagan <generamics@gmail.com>
2022-06-18 14:06:27 +0100

    Corrected links

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 
 import classes from '../styles/Home.module.css'
 
@@ -22,9 +23,9 @@ export default function Home() {
     <p>I&apos;m thinking this will help you to pass those relaxing evenings in your apartment, sketching and painting away on your iPad! It should work well as an accompaniment to your book (which will be waiting for you when you get back home).</p>
     <p className={classes.last}>Hope you have an ace day. Love you. Al xxx</p>
     <div className={classes.buttons}>
-    <button href="https://pay.monzo.com/#F04EGRf_rHpfTiup1FH8WjIOge2MODAqZLWyNwdYr6Sn558nyzJteL25Ng%3D%3D">Money from me</button>
+    <Link external  target="_blank" href="https://pay.monzo.com/#F04EGRf_rHpfTiup1FH8WjIOge2MODAqZLWyNwdYr6Sn558nyzJteL25Ng%3D%3D">Money from me</Link>
     <p className={classes.topay}>to pay for</p>
-    <button href="https://www.udemy.com/share/1013fe/">whatever you fancy from here</button>
+    <Link external target="_blank" href="https://www.udemy.com/share/1013fe/">whatever you fancy from here</Link>
     </div>
   </div>
 </div>
